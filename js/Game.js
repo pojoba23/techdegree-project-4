0fa6b9366972be79d79ac55801b71216e548e305
pojoba23 <62113047+pojoba23@users.noreply.github.com>
2020-06-16 08:27:14 -0400

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -5,105 +5,118 @@
 class Game {
     constructor() {
         this.missed = 0;
-        this.phrases = [
-            "get er done",
-            "get to the choppa",
-            "lessa go",
-            "are you ready",
-            "ready or not"
-        ];
+        this.phrases = this.createPhrases();
         this.activePhrase = null;
-    }
-// Add a new phrase to phrase array.
-createPhrases(phrase) {
-    this.phrases.push(phrase)
-}
-// Random phrase added to Phrase.
-getRandomPhrase() {
-    const random = Math.floor(Math.random() * 5);
-    const randomPhrase = game.phrases[random].phrase;
-    return new Phrase(randomPhrase);
-}
-// Hides the start screen overlay, and will reset settings from previous game.
-startGame() {
-    overlay.style.display = 'none';
-    phrase = this.getRandomPhrase();
-    phrase.addPhraseToDisplay();
-    this.activePhrase = phrase;
-    for (let i = 0; i < qwertyKeys.length; i++) {
-        qwertyKeys[i].className = 'key';
-        qwertyKeys[i].disabled = false;
-    }
-    for (let i = 0; i < goodHeart.length; i++) {
-        goodHeart[i].firstElementChild.src = 'images/liveHeart.png';
-        goodHeart[i].firstElementChild.className = '';
-    }
-}
-// Determines if entire phrase has been revealed, if true the gameOver().
-checkForWin() {
-    let missingLetters = 0;
-        for (let i = 0; i < phraseDivUl.length; i++) {
-            if (phraseDivUl[i].className == "hide letter") {
-                missingLetters += 1;
-            }
-        }
-        if (missingLetters === 0) {
-            this.gameOver(true);
-            return true
-        } else {
-            return false
-        }
-}
-// Removes heart; 5 wrong guesses and its gameOver().
-removeLife() {
-    goodHeart[this.missed ].firstElementChild.src='images/lostHeart.png';
-    goodHeart[this.missed].firstElementChild.className= 'animate__animated animate__jello';
-    if (this.missed === 4) {
-        this.gameOver(false);
-    }
-    this.missed += 1;
-}
-/*	Displays green 'win' screen or red 'lose' screen.
-To get the boolean parameter to work:
-https://stackoverflow.com/questions/58442670/javascript-how-to-check-if-a-false-boolean-parameter-is-passed-in-a-function
-*/
-gameOver(gameWon) {
-    if (gameWon !== undefined && !gameWon) {
-        overlay.querySelector('h1').style.color = 'firebrick';
-        overlay.querySelector('h1').textContent = 'Doh! 😖';
-        overlay.className = 'lose';
-        overlay.style.display = '';
-        btn.disabled = false;
-    }
-    if (gameWon) {
-        overlay.querySelector('h1').textContent = '🥳 Woo Hoo!'
-        overlay.querySelector('h1').style.color = 'blueviolet'
-        overlay.className = 'win';
-        overlay.style.display = '';
-        btn.disabled = false;
-    }
-}
-// Pushes letter to checkLetter()
-handleInteraction(button) {
-    let isChecked = phrase.checkLetter(button);
-    if (isChecked !== undefined && !isChecked) {
-        this.removeLife();
-        for (let i = 0; i < qwertyKeys.length; i++) {
-            if (button === qwertyKeys[i].textContent) {
-                qwertyKeys[i].className = 'wrong animate__animated animate__headShake';
-                qwertyKeys[i].disabled = true
-            }
-        }
-    } else {
-        phrase.showMatchedLetter(button);
-        this.checkForWin();
-        for (let i = 0; i < qwertyKeys.length; i++) {
-            if (button === qwertyKeys[i].textContent) {
-                qwertyKeys[i].className = 'chosen animate__animated animate__headShake';
-                qwertyKeys[i].disabled = true;
-            }
-        }
-    }
-}
+    };
 
-}
+    // Creates phrases for use in the game
+    // @return {array} - array of phrases that could be used in the game
+    createPhrases() {
+        const phraseStrings = [
+            'A fish out of water',
+            'Silence is golden',
+            'Two peas in a pod',
+            'Cool as a cucumber',
+            'All bark and no bite'
+        ];
+
+        const phrasesArray = phraseStrings.map(string => new Phrase(string));
+ 		return phrasesArray;
+    };
+
+    // Randomly selects a phrase from the phrases array.
+    // @return {object} - phrase object chosen
+    getRandomPhrase() {
+        const randomPhrase = this.phrases[Math.floor(Math.random() * this.phrases.length)];
+        return randomPhrase;
+    }; 
+
+    // Begins game by selecting a random phrase and displaying it to the player.
+    startGame() {
+        const overlayDiv = document.querySelector('#overlay');
+        overlayDiv.style.display = 'none';
+
+        this.activePhrase = this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+    }; 
+
+    // Checks to see if the button clicked by the player matches a letter in the phrase, 
+    // and then directs the game based on a correct or incorrect guess.
+    // @param {HTMLButtonElement} button - the clicked button element
+    handleInteraction(button) {
+        let letter = button.innerText;
+
+        if(this.activePhrase.checkLetter(letter)) {
+            button.disabled = true;
+            button.classList.add('chosen');
+            this.activePhrase.showMatchedLetter(letter);
+            
+            if(this.checkForWin()) {
+                this.gameOver(true);
+            };
+        } else {
+            button.classList.add('wrong');
+            if(button.disabled !== true) {
+                button.disabled = true;
+                this.removeLife();
+            };
+        };
+    };
+
+    // Increases the value of the missed property, and removes a life from the scoreboard. 
+    removeLife() {
+        const hearts = document.querySelectorAll('.tries img');
+        hearts[this.missed].src = 'images/lostHeart.png';
+        
+        // Also ends game once player is out of lives.
+        this.missed += 1; 
+        if(this.missed >= 5) {
+            this.gameOver(false);
+        };
+    };
+
+    // Checks for winning move by seeing if the player has revealed all of the letters in the active phrase.
+    // @return {boolean} - true if game was won / false if game was lost
+    checkForWin() {
+        const hiddenLetters = document.querySelectorAll('li.hide');
+        return hiddenLetters.length === 0;
+    };
+
+    // Displays game over message and resets gameboard between games.
+    // param {boolean} gameWon - whether or not the player won
+    gameOver(gameWon) {
+        const overlayDiv = document.querySelector('#overlay');
+        overlayDiv.style.display = '';
+        
+        const gameOverMsg = document.querySelector('#game-over-message');
+        if(gameWon) {
+            gameOverMsg.textContent = 'Congrats - you did it! Try another one?';
+            overlayDiv.classList.remove('start');
+            overlayDiv.classList.remove('lose');
+            overlayDiv.classList.add('win');
+        } else {
+            gameOverMsg.textContent = 'Sorry - you ran out of lives. Try again next time!';
+            overlayDiv.classList.remove('start');
+            overlayDiv.classList.remove('win');
+            overlayDiv.classList.add('lose');
+        };
+
+        const phraseLis = document.querySelectorAll('#phrase li');
+        phraseLis.forEach(li => {
+            li.remove();
+        });
+
+        const keyboardButtons = document.querySelectorAll('#qwerty button');
+        keyboardButtons.forEach(button => {
+            button.disabled = false;
+            button.classList.remove('chosen');
+            button.classList.remove('wrong');
+            button.classList.add('key');
+        });
+
+        const hearts = document.querySelectorAll('.tries img');
+        hearts.forEach(heart => {
+            heart.src = 'images/liveHeart.png';
+        });
+    };
+ };
